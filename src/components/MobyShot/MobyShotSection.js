38a0d0669254f95116b0e0db71a3af56d80c6627
kyle-mucerino <kyle.mucerino@gmail.com>
2024-11-13 10:14:15 -0800

    moby shot mobile grid

--- src/components/MobyShot/MobyShotSection.js
+++ src/components/MobyShot/MobyShotSection.js
@@ -9,10 +9,10 @@ import sex1 from "../../assets/Group.png";
 import expandingArrows from "../../assets/expanding-arrows.png";
 
 const MobyShotSection = () => (
-  <div className="moby-shot flex flex-col items-start mx-[156.44px] my-[164px] inter-semibold">
-    <div className="frame-114 flex flex-col items-start">
+  <div className="moby-shot flex flex-col items-start sm:mx-[156.44px] sm:my-[164px] inter-semibold p-4">
+    <div className="frame-114 flex flex-col items-start max-[400px]:p-4">
       <div className="header-group text-left mb-4">
-        <h2 className="header-gradient inter-semibold mb-[42px] text-[72px] sm:text-[164px]">
+        <h2 className="header-gradient inter-semibold sm:mb-[42px] text-[72px] sm:text-[164px]">
           Moby Shot™
         </h2>
         <p className="text-[36px] sm:text-[64px] inter-semibold">
@@ -22,7 +22,7 @@ const MobyShotSection = () => (
       <div className="image-container w-full overflow-hidden">
         <img src={prp} alt="Prp Tube" className="w-full h-full object-cover" />
       </div>
-      <div className="rich-text leading-relaxed inter-semibold text-[24px] sm:text-[48px]">
+      <div className="rich-text leading-relaxed inter-semibold text-[24px] sm:text-[48px] text-left">
         <p>
           Hyper PRP™ is derived from your own blood and contains an abundance of
           growth factors.
@@ -30,15 +30,20 @@ const MobyShotSection = () => (
       </div>
     </div>
     {/* Benefits Grid would go here */}
-    <div className="benefits mx-auto w-[1115px]">
+    <div className="benefits max-[400px]:p-4">
       {/* Benefits Subheader */}
-      <h3 className="benefits-subheader text-left text-[48px] mb-[92px]">
-        The growth factors in Hyper PRP™ <br></br> helps you:
+      <h3 className="benefits-subheader text-left text-[24px] sm:text-[48px] sm:mb-[92px]">
+        The growth factors in Hyper PRP™
+        <span className="hidden sm:inline">
+          {" "}
+          <br />
+        </span>
+        helps you:
       </h3>
 
       {/* Benefits Grid */}
       <div
-        className="grid grid-cols-3 grid-rows-2 gap-8"
+        className="sm:grid grid-cols-3 grid-rows-2 gap-8 hidden"
         style={{ height: "728.79px" }}
       >
         {/* Grid Item 1 */}
@@ -89,11 +94,73 @@ const MobyShotSection = () => (
         </div>
       </div>
     </div>
-    <div className="benefits-bottom-cta flex flex-col text-left sm:mt-[116px]">
-      <p className="benefits-bottom">
+
+    {/* Mobile Grid */}
+    <div className="grid grid-cols-2 gap-4 sm:hidden p-4">
+      {/* Grid Item 1 */}
+      <div className="mobile-grid-item bg-gray-200 flex flex-col items-center justify-center shadow-md p-4">
+        <img src={leaves} alt="Benefit 1" className="benefit-img mb-2" />
+        <p className="text-center font-medium text-[16px]">
+          Grow new blood vessels
+        </p>
+      </div>
+      {/* Grid Item 2 */}
+      <div className="mobile-grid-item bg-gray-200 flex flex-col items-center justify-center shadow-md p-4">
+        <img
+          src={faucet}
+          alt="Faucet with water flowing"
+          className="benefit-img mb-2"
+        />
+        <p className="text-center font-medium text-[16px]">
+          Increase blood flow
+        </p>
+      </div>
+      <div className="mobile-grid-item bg-gray-200 flex flex-col items-center justify-center shadow-md p-4">
+        <img
+          src={grid3}
+          alt="Faucet with water flowing"
+          className="benefit-img mb-2"
+        />
+        <p className="text-center font-medium text-[16px]">
+          Heal and restore targeted tissue
+        </p>
+      </div>
+      <div className="mobile-grid-item bg-gray-200 flex flex-col items-center justify-center shadow-md p-4">
+        <img
+          src={hammer}
+          alt="Faucet with water flowing"
+          className="benefit-img mb-2"
+        />
+        <p className="text-center font-medium text-[16px]">
+          Get harder erections
+        </p>
+      </div>
+      {/* Grid Item 5 */}
+      <div className="mobile-grid-item relative flex items-center justify-center bg-gray-200 shadow-md">
+        {/* Image */}
+        <img src={expandingArrows} alt="Expanding arrows" className="" />
+
+        {/* Overlay Text */}
+        <p className="absolute text-center text-[16px]">
+          Grow new nerves for heightened sensations
+        </p>
+      </div>
+      <div className="mobile-grid-item bg-gray-200 flex flex-col items-center justify-center shadow-md p-4">
+        <img
+          src={sex1}
+          alt="Faucet with water flowing"
+          className="benefit-img mb-2"
+        />
+        <p className="text-center font-medium text-[16px]">
+          Increase blood flow
+        </p>
+      </div>
+    </div>
+    <div className="benefits-bottom-cta flex flex-col text-left sm:mt-[116px] p-4">
+      <p className="benefits-bottom text-[20px] sm:text-[48px]">
         Each <span> Moby Shot™ </span>
         procedure takes about <span> 60 minutes </span>
-        to complete.Patients often report more
+        to complete. Patients often report more
         <span>noticeable results with two procedures.</span>
       </p>
       <div className="benefits-cta flex flex-row">
